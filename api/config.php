6504c02c6e69c61287d71cfb8dccf3e78c54d956
Kevin David <66553665+kevindavidk@users.noreply.github.com>
2020-06-07 19:51:22 +0600

    Add files via upload

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', '1Ww3NAlgvmGglcH0a2kHzczCh');
+    define('CONSUMER_SECRET', 'X4wQfEq3PzkIH1M3j5FTN8luqnD0IHVoUZXsp6ELp9dnRPblck');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '101839460-G5wJWvufzP2mxFBOEOw1PEkfF7McIoTF748wXC7R');
+    define('ACCESS_SECRET', 'oqTc0gy2dmLP4mKmK974ZFJH3UuBgzaC44JfGl4dmDRKW'); 
